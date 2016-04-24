734e8fb758f6abd8839933ee2dc258e90f841894
tyzbit <tyzbit@gmail.com>
2016-04-23 21:20:21 -0400

    Initial code commit

---
+++ recontainer.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# recontainer
+
+function help {
+cat <<EOF
+$(basename "$0"): go through the current directory and transfer any h264/aac videos to MP4, preserving subtitles if possible
+
+ -c doesn't prompt to convert
+ -d doesn't prompt to delete
+ -i [file] converts a single file"
+EOF
+}
+
+mp4ify ()
+{
+    ffmpeg -i "$1" -c:v copy -c:a copy -c:s mov_text "${1%.*}.mp4"
+}
+convert=''
+delete=''
+
+while getopts ":cdi:fh" opt; do
+  case $opt in
+    c)
+      echo "autoconverting"
+      convert=true
+      ;;
+    d)
+      delete=true
+      ;;
+	i)
+	  echo "single file mode, converting $OPTARG"
+	  mp4ify "$OPTARG"
+	  exit 0
+	  ;;
+	h)
+	  help
+	  exit 0
+	  ;;
+	*)
+	  echo "Unknown switch, exiting"
+	  exit 1
+	  ;;
+  esac
+done
+
+for i in *.mkv; do 
+	ffmpeg -i "$i" 2>&1 | grep -q 'Audio: aac'
+	if [ $? -eq 0 ]; then 
+		ffmpeg -i "$i" 2>&1 | grep -q "Video: h264"
+		if [ $? -eq 0 ]; then
+			if [ "$convert" != true ]; then
+				echo "convert $i?"
+				read p
+			fi
+			if [[ ${p,,} == y* ]] || [ "$convert" = true ]; then 
+				mp4ify "$i"
+			fi
+			unset p
+			if [ "$delete" != true ]; then
+				echo "delete $i?"
+				read p
+			fi
+			if [[ ${p,,} == y* ]] || [ "$delete" = true ]; then 
+				rm "$i"
+			fi
+		fi
+	fi
+done
